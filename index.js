24dfdfcda9193c9ad19cb30e2d1e9b893c48987c
Ajay <sumitpadadune1689@gmail.com>
2024-06-30 22:34:58 +0530

    4th

--- index.js
+++ index.js
@@ -58,7 +58,7 @@ const server = app.listen(
 const io = require("socket.io")(server, {
   pingTimeout: 60000,
   cors: {
-    origin: "https://chat-app-frontend-xi-six.vercel.app/",
+    origin: "https://chat-app-frontend-xi-six.vercel.app",
     // credentials: true,
   },
 });
